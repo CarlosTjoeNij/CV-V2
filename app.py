5fad0483cd6a9b6c90b0e3b4d44fbd96fbe457b8
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-07-07 09:44:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import time
 import pandas as pd
-# from selenium import webdriver
-import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
