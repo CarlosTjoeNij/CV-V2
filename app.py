ead428502ed70f9468a74f216f76930164d1b2f3
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-07-07 10:57:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -82,7 +84,7 @@ def scrape_jobs():
     options.add_argument("--disable-dev-shm-usage")
 
     # Als chromium en chromedriver standaard in PATH staan via nix, hoef je geen pad aan te geven.
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     wait = WebDriverWait(driver, 10)
 
     # Inloggen
