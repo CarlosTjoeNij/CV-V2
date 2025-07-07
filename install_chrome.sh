1ef527142cd8935a11bd0a9ecedaaaf57dcc4724
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-07-07 09:36:02 +0200

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,9 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Installeer dependencies
-apt-get update
-apt-get install -y wget unzip xvfb libxi6 libgconf-2-4 libnss3 libxss1 libappindicator1 libindicator7 libgbm-dev
+set -e
+
+# Update en installeer dependencies
+apt-get update && apt-get install -y \
+    ca-certificates \
+    fonts-liberation \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libdrm2 \
+    libgbm1 \
+    libnspr4 \
+    libnss3 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    xdg-utils \
+    wget \
+    gnupg \
+    lsb-release
+
+# Voeg Google’s sleutel toe
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /usr/share/keyrings/google.gpg
+
+# Voeg de Chrome repository toe
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
 # Installeer Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+apt-get update && apt-get install -y google-chrome-stable
