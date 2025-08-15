52dc3e47cb10df033cc70a2795f8ea5229573283
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-08-15 15:09:39 +0200

    Update daily_scraper.py

--- daily_scraper.py
+++ daily_scraper.py
@@ -3,10 +3,9 @@ import datetime
 import pandas as pd
 import io
 from google.cloud import storage
-from app import scrape_all_jobs  # importeer je bestaande functie
+from scraper_core import scrape_all_jobs
 
 def upload_df_to_gcs(df: pd.DataFrame, bucket_name: str, blob_name: str):
-    """Upload dataframe als Parquet naar Google Cloud Storage."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
