87dea76095ee4e1e26dd25144c361973e3d04fb4
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-10 11:41:14 +0200

    Update scraper_core.py

--- scraper_core.py
+++ scraper_core.py
@@ -22,14 +22,18 @@ FLEX_PASS = st.secrets["flextender"]["password"]
 def get_chrome_driver(timeout=15):
     chrome_options = Options()
     chrome_options.binary_location = "/usr/bin/google-chrome"
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless=new")  # nieuw headless mode
+    chrome_options.add_argument("--no-sandbox")    # verplicht voor Cloud Run
+    chrome_options.add_argument("--disable-dev-shm-usage")  # voorkomt crashes bij weinig /dev/shm
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--remote-debugging-port=9222")
 
     service = Service("/usr/bin/chromedriver")
     print("✅ Start ChromeDriver vanaf:", service.path)
+
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.implicitly_wait(timeout)
     return driver
