6cd1d9f50b02b18e8a31b09b8a035496eb5e6630
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-11 11:16:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import io
 import datetime
 from google.cloud import storage
+import fitz
 
 # Titel & intro
 st.set_page_config(page_title="CV Matcher", layout="wide")
@@ -86,6 +87,7 @@ if uploaded_file:
                 st.write(f"- {word} (score: {score:.3f})")
         else:
             st.info("Upload eerst een CV om de matching te starten.")
+
 
 
 
