ee0a288e288623013a0ad8c30eef343d80b7f456
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-15 14:48:21 +0200

    Update flextender.py

--- platformen/flextender.py
+++ platformen/flextender.py
@@ -3,6 +3,7 @@ import time
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
