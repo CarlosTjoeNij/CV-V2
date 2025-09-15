fb2573360a6e3162fdc8fdc0a1cc87069d6ecc05
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2025-09-15 14:20:53 +0200

    Update yacht.py

--- platformen/yacht.py
+++ platformen/yacht.py
@@ -44,6 +44,18 @@ provincies = [
     "Limburg"
 ]
 
+def get_chrome_driver(timeout=15):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    driver = webdriver.Chrome(options=chrome_options)  # Selenium Manager regelt driver
+    driver.implicitly_wait(timeout)
+    return driver
+
 def scrape_yacht():
     options = Options()
     options.add_argument("--headless")
@@ -52,7 +64,7 @@ def scrape_yacht():
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")
     
-    driver = webdriver.Chrome(options=options)
+    driver = get_chrome_driver()
     wait = WebDriverWait(driver, 15)
     
     try:
